a7eb4e55622c847490c489d2d4706302baa92d8b
Fred Zaw <fred.zaw@gmail.com>
2019-04-17 15:17:07 -0700

    increased homepage menu image sizes

--- assets/built/project-menu.js
+++ assets/built/project-menu.js
@@ -33,7 +33,7 @@
 
     // Window size
     let winsize;
-    const calcWinsize = () => winsize = {width: window.innerWidth, height: document.documentElement.scrollHeight - 200};
+    const calcWinsize = () => winsize = {width: window.innerWidth, height: document.documentElement.scrollHeight};
     calcWinsize();
     window.addEventListener('resize', calcWinsize);
 
